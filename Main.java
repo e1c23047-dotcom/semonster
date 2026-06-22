79f83696ca107e9e9e0cf215ceed76010bbb6deb
e1c23047 <e1c23047@oit.ac.jp>
2026-06-22 10:34:27 +0900

    add player

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("You are not SEMonster");
+    System.out.println("SEMonster!");
+    Player player = new Player();
+    player.drawMonsters();
+    player.showDeck();
   }
 }
